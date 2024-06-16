c13d387b2856fe52cd78d0089fac93a4418d4fc4
Mihail Slanin <mslaninwork@gmail.com>
2024-06-16 19:58:12 +0400

    Initial Commit

---
+++ SabrInvest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SabrInvest
+//
+//  Created by Mihail Slanin on 16.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
